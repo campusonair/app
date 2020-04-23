f729633d8bb3f06ea5a280eaf917d21ad8d2fb73
naoki <n.globe.us@gmail.com>
2020-04-24 06:29:23 +0900

    change live stream url to cloudfront

--- src/VideoContainer.tsx
+++ src/VideoContainer.tsx
@@ -5,7 +5,7 @@ const videoJsOptions = {
   sources: [
     {
       src:
-        "https://15bcd044c531aa99.mediapackage.ap-northeast-1.amazonaws.com/out/v1/33de575936f14b54b4c642d4c0d2aadc/index.m3u8",
+        "https://d3mjgtdn3omat8.cloudfront.net/out/v1/33de575936f14b54b4c642d4c0d2aadc/index.m3u8",
       type: "application/x-mpegURL",
     },
   ],
