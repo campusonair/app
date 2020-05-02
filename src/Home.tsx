acb26ffd3b63182ffee18f514209b719d5b77b64
naoki <n.globe.us@gmail.com>
2020-05-02 15:18:15 +0900

    Add Websocket function

--- src/Home.tsx
+++ src/Home.tsx
@@ -11,58 +11,37 @@ import { IoIosThumbsUp, IoIosThumbsDown } from "react-icons/io";
 type Props = {};
 
 const Content = (props: Props) => {
+
+  const [message, setMessage] = React.useState("No Message");
   const ws = new WebSocket(
     "wss://bedhbwbhi7.execute-api.ap-northeast-1.amazonaws.com/v1"
   );
   ws.onerror = (error) => {
     console.error(error);
   };
-  ws.onmessage = (message) => {
-    console.log(message);
-  };
+  ws.onmessage = (event) => {
+    const data = JSON.parse(event.data);
+    setMessage(data.message);
+  }
   return (
     <>
       <IoIosThumbsUp
-        onClick={() => {
+        onClick={
           ws.onopen = () => {
             ws.send("thumbsUp");
-          };
-        }}
+          }
+        }
       />
       <IoIosThumbsDown
-        onClick={() => {
-          console.log("thumbsDown");
-        }}
+        onClick={
+          ws.onopen = () => {
+            ws.send("thumbsDown");
+          }
+        }
       />
-      {/* <div className={"home-header"}>
-        <h1>Example World 2020 re:Public</h1>
+      <div>
+        {message}
       </div>
-      <div className={"home-body"}>
-        <main className={"home-main"}>
-          <SlideContainer />
-        </main>
-        <aside className={"home-aside"}>
-          <ChatContainer />
-        </aside>
-      </div>
-      <div className={"home-footer"}>
-        <div className={"home-footer-content"} >
-          <div className={"home-footer-content-left"}>
-            <div className={"home-footer-content-col"}>
-              <LogoContainer />
-            </div>
-            <div className={"home-footer-content-col"}>
-              <QrContainer />
-            </div>
-            <div className={"home-footer-content-col-half"}>
-              <NoticeContainer />
-            </div>
-          </div>
-          <div className={"home-footer-content-right"}>
-            <VideoContainer />
-          </div>
-        </div>
-      </div> */}
     </>
   );
 };
