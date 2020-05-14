d9136e3022374395c1ae24c342462686ab6ce15c
Takayuki Miyauchi <miya0001@users.noreply.github.com>
2020-05-15 02:51:42 +0900

    displays the name on header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,23 @@
 import React from "react";
-import { Navbar, NavDropdown } from 'react-bootstrap'
+import { Navbar, NavDropdown, Button } from 'react-bootstrap'
 import { useAuth0 } from "../react-auth0-spa";
 
 const NavBar = () => {
-  const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
+  const { isAuthenticated, loginWithRedirect, logout, loading, user } = useAuth0();
+  const [userName, setUserName] = React.useState<string>('')
+
+  React.useEffect(() => {
+    if (isAuthenticated && (!loading || user)) {
+      setUserName(user.nickname)
+    }
+  }, [loading, user, isAuthenticated])
 
   return (
     <Navbar className="justify-content-between">
       <Navbar.Brand href="/">React-Bootstrap</Navbar.Brand>
-      {!isAuthenticated && (<button onClick={() => loginWithRedirect({})}>Log in</button>)}
-      {isAuthenticated &&
-        <NavDropdown title="Dropdown" id="basic-nav-dropdown" alignRight>
+      {!isAuthenticated && (<Button onClick={() => loginWithRedirect({})}>Log in</Button>)}
+      {userName &&
+        <NavDropdown title={userName} id="basic-nav-dropdown" alignRight>
           <NavDropdown.Item href="/profile">Profile</NavDropdown.Item>
           <NavDropdown.Divider />
           <NavDropdown.Item onClick={() => logout()}>Logout</NavDropdown.Item>
