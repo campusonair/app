68e16f25abb92961fdae6467f3a19eb978ff60b9
naoki <n.globe.us@gmail.com>
2020-05-19 20:33:53 +0900

    some fix

--- src/components/Live.tsx
+++ src/components/Live.tsx
@@ -1,14 +1,78 @@
 import * as React from "react";
-import { Container } from 'react-bootstrap'
+import { Container, Row, Col } from 'react-bootstrap'
 import { useParams } from "react-router-dom"
+import Video from './Video'
+import LiveCanvas from './Studio/LiveCanvas'
+import Peer from 'skyway-js'
+import Config from '../config'
+
+import './Studio.scss'
 
 type Props = {};
 
 const Content = (props: Props) => {
   const { liveId } = useParams()
 
+  const videoOptions = {
+    video: {
+      width: 1280,
+      height: 720
+    },
+    audio: true
+  }
+
+  const userMedia = navigator.mediaDevices.getUserMedia(videoOptions)
+  const guestVideos = React.useRef<HTMLDivElement>(null);
+  const peer = new Peer({ key: Config.skyWayApiKey });
+
+  // userMedia.then(localStream => {
+
+  //   peer.on('open', () => {
+
+  //     const room = peer.joinRoom(liveId!, {
+  //       mode: 'sfu',
+  //       stream: localStream,
+  //     });
+
+  //     //参加者が追加されると呼び出し
+  //     room.on('stream', async stream => {
+
+  //       if (stream.peerId === peer.id) {
+  //         return
+  //       }
+  //       const newVideoContainer = document.createElement('div');
+  //       newVideoContainer.className = 'video ';
+  //       // //ビデオタグを作成
+  //       const newVideo = document.createElement('video');
+  //       newVideoContainer.appendChild(newVideo);
+  //       //参加者の動画をセット
+  //       newVideo.srcObject = stream;
+  //       //離脱時のIDとして、peerIdをセット
+  //       newVideo.setAttribute('data-peer-id', stream.peerId);
+  //       //ビデオタグを追加
+  //       guestVideos.current!.append(newVideoContainer);
+  //       //再生開始
+  //       await newVideo.play().catch(console.error);
+  //     });
+  //   })
+  // })
+
   return (
-    <Container>Live id is {liveId}</Container>
+    <Container>
+      <Row>
+        <Col xs={9}>
+          <div className="videos-container">
+            <div className="me videos">
+              <Video media={userMedia} />
+              <div>hello</div>
+            </div>
+            <div className="guests videos" ref={guestVideos} />
+          </div>
+        </Col>
+        <Col xs={3}>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
--- src/components/Studio.tsx
+++ src/components/Studio.tsx
@@ -3,6 +3,8 @@ import { Container, Row, Col } from 'react-bootstrap'
 import { useParams } from "react-router-dom"
 import Video from './Video'
 import LiveCanvas from './Studio/LiveCanvas'
+import Peer from 'skyway-js'
+import Config from '../config'
 
 import './Studio.scss'
 
@@ -20,6 +22,38 @@ const Content = (props: Props) => {
   }
 
   const userMedia = navigator.mediaDevices.getUserMedia(videoOptions)
+  const guestVideos = React.useRef<HTMLDivElement>(null);
+  const peer = new Peer({ key: Config.skyWayApiKey });
+  userMedia.then(localStream => {
+
+    peer.on('open', () => {
+
+      const room = peer.joinRoom(liveId!, {
+        mode: 'sfu',
+        stream: localStream,
+      });
+      //参加者が追加されると呼び出し
+      room.on('stream', async stream => {
+
+        if (stream.peerId === peer.id) {
+          return
+        }
+        const newVideoContainer = document.createElement('div');
+        newVideoContainer.className = 'video ';
+        // //ビデオタグを作成
+        const newVideo = document.createElement('video');
+        newVideoContainer.appendChild(newVideo);
+        //参加者の動画をセット
+        newVideo.srcObject = stream;
+        //離脱時のIDとして、peerIdをセット
+        newVideo.setAttribute('data-peer-id', stream.peerId);
+        //ビデオタグを追加
+        guestVideos.current!.append(newVideoContainer);
+        //再生開始
+        await newVideo.play().catch(console.error);
+      });
+    })
+  })
 
   return (
     <Container className="studio">
@@ -35,12 +69,10 @@ const Content = (props: Props) => {
             <div className="me videos">
               <Video media={userMedia} />
             </div>
-            <div className="guests">
-            </div>
+            <div className="guests videos" ref={guestVideos} />
           </div>
         </Col>
         <Col xs={3}>
-
         </Col>
       </Row>
     </Container>
