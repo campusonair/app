ff675aff766ec3f3724fc5c4b4e4e63687733e5f
naoki <n.globe.us@gmail.com>
2020-05-19 21:15:09 +0900

    Add invite button

---
+++ src/components/InviteButton.tsx
@@ -0,0 +1,18 @@
+import * as React from "react";
+import { __ } from '@wordpress/i18n'
+import { useParams } from "react-router-dom"
+
+type Props = {};
+
+const Content = (props: Props) => {
+  const { liveId } = useParams()
+  const [inviteUrl, setInviteUrl] = React.useState<boolean>(false)
+
+  return (
+    <>
+      <button onClick={() => setInviteUrl(!inviteUrl)}>{__("Invite guest")}</button>
+      {inviteUrl && <div>{window.location.origin}/live/{liveId}</div>}
+    </>
+  );
+};
+export default Content;
